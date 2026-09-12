4ece9998fcc9bd650a710a7431e90121de06b4eb
justins-github <hewjustin@gmail.com>
2026-09-12 15:42:57 +0800

    Implement findMax, findDuplicates and findUnique

--- NumbersArray.java
+++ NumbersArray.java
@@ -20,6 +20,71 @@ public class NumbersArray {
     }
 
     // Write your methods here
-    
-}
+    public static int findMax(Integer[] input)
+    {
+        int currentHighest = Integer.MIN_VALUE;
+        for (int i = 0; i < input.length; i++)
+        {
+            if (input[i] > currentHighest)
+            {
+                currentHighest = input[i];
+            }
+        }
+        return currentHighest;
+    }
 
+    public static Integer[] findDuplicates(Integer[] input)
+    {
+        Integer[] duplicates = new Integer[input.length];
+        boolean hasDupe;
+        int dupCounter = 0;
+
+        for (int i = 0; i < input.length; i++)
+        {
+            boolean seenBefore = false;
+            for (int j = 0; j < i; j++)
+            {
+                if (input[i].equals(input[j])) seenBefore = true;
+            }
+            if (seenBefore) continue;
+
+            hasDupe = false;
+            for (int j = i + 1; j < input.length; j++)
+            {
+                if (input[i].equals(input[j])) hasDupe = true;
+            }
+            if (hasDupe)
+            {
+                duplicates[dupCounter] = input[i];
+                dupCounter++;
+            }
+        }
+
+        return Arrays.copyOf(duplicates, dupCounter);
+        
+    }
+
+    public static Integer[] findUnique(Integer[] input)
+    {
+        Integer[] uniqueNums = new Integer[input.length];
+        boolean hasDupe;
+        int uniqueCount = 0;
+
+        for (int i = 0; i < input.length; i++)
+        {
+            hasDupe = false;
+            for (int j = 0; j < input.length; j++)
+            {
+                if (i == j) continue; // same index - doesn't count
+                if (input[i].equals(input[j])) hasDupe = true;
+            }
+            if (!hasDupe)
+            {
+                uniqueNums[uniqueCount] = input[i];
+                uniqueCount++;
+            }
+        }
+
+        return Arrays.copyOf(uniqueNums, uniqueCount);
+    }
+}
